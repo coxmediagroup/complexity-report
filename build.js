abfbaccdd0a49ad929600857e2af07395dc0f07f
Dan Cobb <cobbdb@gmail.com>
2017-02-18 16:53:03 -0800

    Support title. Add readme.

--- build.js
+++ build.js
@@ -4,17 +4,21 @@ let chalk = require('chalk'),
 
 // Validate command.
 if (!argv[2]) {
-    console.log(chalk.red('Missing source path!\n'));
-    console.log('%s node build.js %s path/to/dest', chalk.bgWhite.black('USAGE>'), chalk.yellow('path/to/source'));
+    console.log(chalk.red('Missing report title!\n'));
+    console.log('%s node build.js %s path/to/source path/to/dest', chalk.bgWhite.black('USAGE>'), chalk.yellow('title'));
     return;
 } else if (!argv[3]) {
+    console.log(chalk.red('Missing source path!\n'));
+    console.log('%s node build.js title %s path/to/dest', chalk.bgWhite.black('USAGE>'), chalk.yellow('path/to/source'));
+    return;
+} else if (!argv[4]) {
     console.log(chalk.red('Missing output path!\n'));
-    console.log('%s node build.js path/to/source %s', chalk.bgWhite.black('USAGE>'), chalk.yellow('path/to/dest'));
+    console.log('%s node build.js title path/to/source %s', chalk.bgWhite.black('USAGE>'), chalk.yellow('path/to/dest'));
     return;
 }
 
 // Run the build.
-plato.inspect(argv[2], argv[3], {
-    title: 'NP-Paid Complexity Report',
+plato.inspect(argv[3], argv[4], {
+    title: argv[2],
     eslint: require('./eslint.json')
 });
--- readme.md
+++ readme.md
@@ -1,1 +1,19 @@
- 
+# Plato Complexity Report
+
+[![Dependencies](https://img.shields.io/david/coxmediagroup/complexity-report.svg)](./package.json)
+
+-------------
+
+### Setup
+```sh
+git clone https://github.com/coxmediagroup/complexity-report.git
+```
+
+### Running
+```sh
+node complexity-report/build.js "Site Complexity Report" my-project/src/**/*.js public_html/site-report
+```
+
+---------
+* See: https://github.com/coxmediagroup/complexity-report
+* License: MIT
